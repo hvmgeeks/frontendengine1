006157c58f7012631d9f17d8e79c71cd9886cf1d
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-24 21:27:56 +0300

    Update index.js
    adding the base url to the index.js

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+    baseURL: 'http://16.16.56.238:5000',
     headers: {
          Authorization : `Bearer ${localStorage.getItem('token')}`
     }
