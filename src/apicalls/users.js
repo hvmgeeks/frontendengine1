991b5a93c784d22fc4f20a5aff963c8c56a2cd92
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-26 14:01:42 +0300

    Update users.js
    updated

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -16,9 +16,28 @@ export const loginUser = async (payload) => {
         console.log(response);
         return response.data;
     } catch (error) {
-        return error.response.data;
+        // Log the error for debugging.
+        console.error(error);
+
+        // Check if error.response is defined.
+        if (error.response) {
+            return error.response.data;
+        } else {
+            // Handle error differently if error.response is undefined.
+            return { error: 'Network error or server is unreachable' };
+        }
     }
 }
+
+// export const loginUser = async (payload) => {
+//     try {
+//         const response = await axiosInstance.post('/api/users/login', payload);
+//         console.log(response);
+//         return response.data;
+//     } catch (error) {
+//         return error.response.data;
+//     }
+// }
 
 export const getUserInfo = async () => {
     try {
