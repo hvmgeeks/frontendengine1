8dfd815510477dbf130f71c19c518b30359d2e62
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-26 14:07:25 +0300

    Update users.js
    updated apicalls users

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -1,14 +1,5 @@
 const { default: axiosInstance } = require(".");
 
-export const registerUser = async (payload) => {
-    try {
-        const response = await axiosInstance.post('/api/users/register', payload);
-        console.log(response);
-        return response.data;
-    } catch (error) {
-        return error.response.data;
-    }
-}
 
 export const loginUser = async (payload) => {
     try {
@@ -16,18 +7,52 @@ export const loginUser = async (payload) => {
         console.log(response);
         return response.data;
     } catch (error) {
-        // Log the error for debugging.
         console.error(error);
-
-        // Check if error.response is defined.
         if (error.response) {
             return error.response.data;
         } else {
-            // Handle error differently if error.response is undefined.
             return { error: 'Network error or server is unreachable' };
         }
     }
 }
+
+export const registerUser = async (payload) => {
+    try {
+        const response = await axiosInstance.post('/api/users/register', payload);
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        if (error.response) {
+            return error.response.data;
+        } else {
+            return { error: 'Network error or server is unreachable' };
+        }
+    }
+}
+In the code above, axiosInstance is assumed to be an instance of Axios that you have configured. It's used to send HTTP requests to your API server.
+
+2. Using the registerUser and loginUser Functions:
+Below is an example of how you can use these functions in your component or wherever you need to call them. It includes error handling to ensure that your application can gracefully handle and display errors to the user.
+
+javascript
+Copy code
+// ExampleComponent.js
+import React, { useState } from 'react';
+import { loginUser, registerUser }
+
+
+
+// export const registerUser = async (payload) => {
+//     try {
+//         const response = await axiosInstance.post('/api/users/register', payload);
+//         console.log(response);
+//         return response.data;
+//     } catch (error) {
+//         return error.response.data;
+//     }
+// }
+
 
 // export const loginUser = async (payload) => {
 //     try {
