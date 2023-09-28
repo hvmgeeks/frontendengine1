885f83177039236435c60f162575f9673943f472
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-28 21:52:36 +0300

    Update index.js
    ---

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://stjosephkibadaengine.com',
+    baseURL: https://backend-r0tz.onrender.com',
     headers: {
          Authorization : `Bearer ${localStorage.getItem('token')}`
     }
