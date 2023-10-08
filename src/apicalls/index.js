7a024c75c1f644c0f133567fb154e76c7b0fdab1
hvm geeks <hvmgeeks@gmail.com>
2023-10-08 13:28:47 +0300

    new changes

---
+++ src/apicalls/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://backend-r0tz.onrender.com',
+    headers: {
+         Authorization : `Bearer ${localStorage.getItem('token')}`
+    }
+});
+
+export default axiosInstance;
