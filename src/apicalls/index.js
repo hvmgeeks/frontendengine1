8eb37c8e8513a3dcd129bc0c7a95131a8c986d0f
hvm geeks <hvmgeeks@gmail.com>
2024-02-01 13:11:10 +0300

    new

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://backend-r0tz.onrender.com' ,
-    baseURL: 'http://localhost:5000' ,
+     baseURL: 'https://backend-r0tz.onrender.com' ,
+    //baseURL: 'http://localhost:5000' ,
     headers: {
         Authorization : `Bearer ${localStorage.getItem('token')}`
     }
