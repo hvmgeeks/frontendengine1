63ec2427e62dcc5e4c971fddfadd7f387b9ea2bf
Your Name <you@example.com>
2024-03-21 15:50:17 -0700

    new changes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -203,7 +203,7 @@ function ProtectedRoute({ children }) {
                 onClick={() => setCollapsed(false)}
               ></i>
             )}
-            <h1 className={`text-white ${isMobile ? 'text-xs' : 'text-2xl'}`}>ST JOSEPH KIBADA ONLINE STUDY APP</h1>
+            <h1 className={`text-white ${isMobile ? 'text-xs' : 'text-2xl'}`}>PLUTONIUM 📚🖊</h1>
             <div>
               <div className="flex gap-1 items-center">
                 <h1 className={`text-white ${isMobile ? 'text-xs' : 'text-md'}`}>{user?.name}</h1>
