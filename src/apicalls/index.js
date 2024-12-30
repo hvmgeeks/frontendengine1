18b063d4fce0f8d47027dd7d64a60508e4379d5d
Your Name <you@example.com>
2024-12-30 16:03:45 +0300

    new12

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
      baseURL: 'https://server-fmff.onrender.com' ,
-    baseURL: 'http://localhost:5000' ,
+    //baseURL: 'http://localhost:5000' ,
     headers: {
         Authorization : `Bearer ${localStorage.getItem('token')}`
     }
